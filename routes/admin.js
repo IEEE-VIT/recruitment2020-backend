ceae19403007303fbda7b9a70012abd278ab1ac5
DarthBenro008 <hkpdev008@gmail.com>
2020-11-23 01:28:22 +0530

    feat: select candidates for interview

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ const adminController = require("../controller/admin.js");
 const round2Controller = require("../controller/round2");
 
 router.get("/r1/candidates", round1Controller.fetchReadyCandidates);
+router.post("/r1/candidates", round1Controller.selectReadyCandidates);
 router.get("/", adminController.readAdmin);
 router.put("/", adminController.updateAdmin);
 router.get("/r2/tech/candidates", adminController.fetchTechRound2Candidates);
