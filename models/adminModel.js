908ca89d278b6b34f6b8a5eb0d2afe8aaba41e1a
ShubhamPalriwala <spalriwalau@gmail.com>
2020-11-24 18:36:07 +0000

    Linting Changes

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,49 +1,51 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../utils/db");
 
-const Admin = sequelize.define("Admin", {
-  auid: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    unique: true,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
+const Admin = sequelize.define(
+  "Admin",
+  {
+    auid: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isAlpha: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    meetLink: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true,
+      },
     },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-      isAlpha: true,
-    },
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-      isEmail: true,
-    },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  meetLink: {
-    type: DataTypes.STRING,
-    validate: {
-      isUrl: true,
-    },
-  }
-},
-{
+  {
     defaultScope: {
       attributes: { exclude: ["password"] },
     },
