f2df62062c3e0e7466622104913e84eabab10ef0
ShubhamPalriwala <spalriwalau@gmail.com>
2020-12-02 04:44:49 +0530

    mend

--- routes/admin.js
+++ routes/admin.js
@@ -44,7 +44,7 @@ router.post(
   validater(schemas.round2SetGda),
   round2Controller.setGda
 );
-router.get("/r3/candidates", isBoard, round3Controller.candidates);
+router.get("/r3/candidates", isBoard, queryFilter, round3Controller.candidates);
 
 router.get("/exceptions", queryFilter, adminController.fetchExceptions);
 router.post(
