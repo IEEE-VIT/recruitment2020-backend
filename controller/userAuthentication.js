7670b0e2aee6bcd83bb850a2691c3cdfe31c21eb
DarthBenro008 <hkpdev008@gmail.com>
2020-12-02 17:44:05 +0530

    feat: forget password route

--- controller/userAuthentication.js
+++ controller/userAuthentication.js
@@ -1,6 +1,13 @@
+/* eslint-disable eqeqeq */
+const moment = require("moment-timezone");
 const authMiddlewaare = require("../middleware/authentication");
 const userModel = require("../models/userModel");
+const forgotPasswordModel = require("../models/forgotPasswordModel");
+const emailer = require("../utils/emailer");
+const db = require("../utils/db");
 const response = require("../utils/genericResponse");
+
+moment.tz.setDefault("Asia/Calcutta");
 
 const login = async (req, res) => {
   const { regNo, password } = req.body;
@@ -58,4 +65,96 @@ const register = async (req, res) => {
     });
 };
 
-module.exports = { login, register };
+const generateOtpAndTime = () => {
+  return {
+    otp: Math.floor(100000 + Math.random() * 900000),
+    time: moment().add(10, "minutes"),
+  };
+};
+
+const forgetPasswordEmailer = async (generatedOtpAndTime, emailId) => {
+  const emailIdParsed = [emailId];
+  const template = "";
+  const emailResult = await emailer(template, emailIdParsed);
+  return emailResult;
+};
+
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+  try {
+    const userData = await userModel.findOne({ where: { email } });
+    if (userData.length === 0) {
+      throw Error("No such email registered!");
+    }
+    const forgotPasswordData = await forgotPasswordModel.findOne({
+      where: { regNo: userData.regNo },
+    });
+    await db.transaction(async (chain) => {
+      if (forgotPasswordData.length === 0) {
+        const generatedCreds = generateOtpAndTime();
+        const newForgotPasswordData = await forgotPasswordModel.create(
+          {
+            regNo: userData.regNo,
+            expiry: generatedCreds.time,
+            otp: generatedCreds.otp,
+          },
+          { transaction: chain }
+        );
+        if (newForgotPasswordData.length === 0) {
+          throw Error("Error generating OTP");
+        }
+        const emailResult = await forgetPasswordEmailer(
+          generatedCreds,
+          userData.emailId
+        );
+        if (!emailResult) {
+          throw Error("Unable to send Email, hence aborted operation.");
+        }
+      } else if (forgotPasswordData.expiry <= moment()) {
+        const generatedCreds = generateOtpAndTime();
+        const resendForgotPasswordUpdate = await forgotPasswordModel.update(
+          {
+            expiry: generatedCreds.time,
+            otp: generatedCreds.otp,
+            resent: false,
+          },
+          { where: { regNo: userData.regNo }, transaction: chain }
+        );
+        if (resendForgotPasswordUpdate == 0) {
+          throw Error("Unable to updat forget password entry");
+        }
+        const emailResult = await forgetPasswordEmailer(
+          generatedCreds,
+          userData.emailId
+        );
+        if (!emailResult) {
+          throw Error("Unable to send Email, hence aborted operation.");
+        }
+      } else {
+        const generatedCreds = generateOtpAndTime();
+        if (forgotPasswordData.resent) {
+          throw Error("Reset OTP twice, please try later after few mins.");
+        } else {
+          const resendForgotPasswordUpdate = await forgotPasswordModel.update(
+            { otp: generatedCreds.otp, resent: true },
+            { where: { regNo: userData.regNo }, transaction: chain }
+          );
+          if (resendForgotPasswordUpdate == 0) {
+            throw Error("Unable to resend OTP");
+          }
+          const emailResult = await forgetPasswordEmailer(
+            generatedCreds,
+            userData.emailId
+          );
+          if (!emailResult) {
+            throw Error("Unable to send Email, hence aborted operation.");
+          }
+        }
+      }
+    });
+  } catch (err) {
+    response(res, false, "", err.toString());
+  }
+};
+
+module.exports = { login, register, forgotPassword };
