efdd0259e31d3cf14d0c04999bfd6e95c0f49a5c
ShubhamPalriwala <spalriwalau@gmail.com>
2020-12-11 01:31:49 +0530

    feat: round1-sendmail-to-readycandidate

--- controller/round1.js
+++ controller/round1.js
@@ -2,9 +2,12 @@
 const { Op } = require("sequelize");
 const userModel = require("../models/userModel");
 const roundModel = require("../models/roundModel");
+const adminModel = require("../models/adminModel");
 const response = require("../utils/genericResponse");
 const db = require("../utils/db");
 const logger = require("../configs/winston");
+const emailer = require("../utils/emailer");
+const templates = require("../utils/templates");
 
 const updateProjectLink = async (req, res) => {
   userModel
@@ -73,13 +76,41 @@ const selectReadyCandidates = async (req, res) => {
           "Unable to update with admin, please resend the candidates list"
         );
       }
+      // eslint-disable-next-line no-await-in-loop
+      const roundModelDetails = await roundModel.findOne({
+        where: { regNo: candidates[i], roundNo: "1" },
+      });
+      if (roundModelDetails.length == 0) {
+        throw Error("No such user found in round 1");
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      const admin = await adminModel.findOne({ where: { auid: adminId } });
+      if (admin.length == 0) {
+        throw Error("No admin found with such auid");
+      }
+
+      // eslint-disable-next-line no-await-in-loop
+      const user = await userModel.findOne({ where: { regNo: candidates[i] } });
+      if (user.length == 0) {
+        throw Error("Unable to find such user");
+      }
+
+      const candidateEmailId = [user.email];
+      const template = templates.round1Interview(user.name, admin.meetLink);
+      // eslint-disable-next-line no-await-in-loop
+      const email = await emailer(template, candidateEmailId);
+      if (!email.success) {
+        throw Error(
+          `Unable to send the email to the candidate because: ${email.error}`
+        );
+      }
     }
     await chainTransaction.commit();
     response(
       res,
       true,
-      "All Candidates have been assigned",
-      "Candidates Selected to interview!"
+      "All Candidates have been assigned and Emails have been sent"
     );
   } catch (err) {
     logger.error(`Failure to selectReadyCandidates due to ${err}`);
