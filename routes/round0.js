1daaf8aab122525cef6ab8e2ef4b73193c0771af
ShubhamPalriwala <spalriwalau@gmail.com>
2020-12-12 01:34:30 +0530

    relocated getAllSlots to Admin

--- routes/round0.js
+++ routes/round0.js
@@ -6,7 +6,6 @@ const schemas = require("../utils/schemas");
 router.get("/slots", round0Controller.getSlots);
 router.get("/question", round0Controller.getQuestions);
 router.get("/verifyslotTime", round0Controller.verifyslotTime);
-router.get("/allslots", round0Controller.getAllSlots);
 
 router.post("/form", validater(schemas.round0form), round0Controller.userForm);
 
