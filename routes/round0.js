31120e5faf573b3640b451dd5fb9473c538d7224
DarthBenro008 <hkpdev008@gmail.com>
2020-12-17 20:09:57 +0530

    fix: enable r0 filledWhy and readd email regex

--- routes/round0.js
+++ routes/round0.js
@@ -1,16 +1,14 @@
 const router = require("express").Router();
 const round0Controller = require("../controller/user/round0");
-const validater = require("../middleware/validation");
-const schemas = require("../utils/schemas");
 
-router.get("/slots", round0Controller.getSlots);
-router.get("/allslots", round0Controller.getAllRound1Slots);
-router.get("/question", round0Controller.getQuestions);
-router.get("/verifyslotTime", round0Controller.verifyslotTime);
-router.get("/filledform", round0Controller.filledFormBoolean);
+// router.get("/slots", round0Controller.getSlots);
+// router.get("/allslots", round0Controller.getAllRound1Slots);
+// router.get("/question", round0Controller.getQuestions);
+// router.get("/verifyslotTime", round0Controller.verifyslotTime);
+// router.get("/filledform", round0Controller.filledFormBoolean);
 router.get("/filledwhy", round0Controller.filledWhyIEEE);
 
 router.post("/why", round0Controller.whyIEEE);
-router.post("/form", validater(schemas.round0form), round0Controller.userForm);
+// router.post("/form", validater(schemas.round0form), round0Controller.userForm);
 
 module.exports = router;
